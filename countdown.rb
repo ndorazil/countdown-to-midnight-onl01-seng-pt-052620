8c819030bc3ddba98a694fb9b04338fa26a2a815
Nathan Dorazil <nathand1915@gmail.com>
2020-05-29 21:13:18 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(time_left)
+  time_left = 10
+  until time_left == 0
+    puts "#{time_left} SECOND(S)!"
+    time_left -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time_left)
+  time_left = 5
+  until time_left == 0
+    "#{time_left} SECOND(S)!"
+    time_left -= 1
+    sleep 1
+  end
 end
